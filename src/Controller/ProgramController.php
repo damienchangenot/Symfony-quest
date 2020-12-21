30d7721a25b1aa77364baa0ac660be2565ebface
Damien Changenot <damien.changenot@gmail.com>
2020-12-21 10:03:36 +0100

    quest 22

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -10,7 +10,9 @@ use App\Entity\Program;
 use App\Entity\Season;
 use App\Form\CommentType;
 use App\Form\ProgramType;
+use App\Form\SearchProgramType;
 use App\Repository\CommentRepository;
+use App\Repository\ProgramRepository;
 use App\Service\Slugify;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -30,15 +32,26 @@ class  ProgramController extends AbstractController
 {
     /**
      * @Route("/", name="index")
+     * @param Request $request
+     * @param ProgramRepository $programRepository
      * @return Response
      */
-    public function index(): Response
+    public function index(Request $request, ProgramRepository $programRepository): Response
     {
-        $programs = $this->getDoctrine()
-            ->getRepository(Program::class)
-            ->findAll();
+        $form = $this->createForm(SearchProgramType::class);
+        $form->handleRequest($request);
 
-        return $this->render('program/index.html.twig', ['programs' => $programs]);
+        if($form->isSubmitted() && $form->isValid()) {
+        $search = $form->getData()['search'];
+        $programs = $programRepository->findLikeName($search);
+    } else {
+        $programs = $programRepository->findAll();
+
+    }
+        return $this->render('program/index.html.twig',
+            ['programs' => $programs,
+             'form' => $form->createView(),
+        ]);
     }
 
     /**
--- src/Repository/ProgramRepository.php
+++ src/Repository/ProgramRepository.php
@@ -19,32 +19,15 @@ class ProgramRepository extends ServiceEntityRepository
         parent::__construct($registry, Program::class);
     }
 
-    // /**
-    //  * @return Program[] Returns an array of Program objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Program
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
+   public function findLikeName(string $name)
+   {
+        $queryBuilder = $this->createQueryBuilder('p')
+            ->join('p.actors', 'a')
+            ->where('a.name LIKE :name')
+            ->orWhere('p.title LIKE :name')
+            ->setParameter('name', '%'. $name . '%')
+            ->orderBy('p.title', 'ASC')
+            ->getQuery();
+        return $queryBuilder->getResult();
+   }
 }
